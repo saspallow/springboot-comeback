8503d7f99f1658638a7b8d416beb4e55f4dfa969
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-10-16 17:25:31 +0700

    Added Error controller.

--- src/main/java/com/saspallow/demo/controller/ErrorControllerImpl.java
+++ src/main/java/com/saspallow/demo/controller/ErrorControllerImpl.java
@@ -1,7 +1,21 @@
 package com.saspallow.demo.controller;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by dt213142 on 10/16/2017.
  */
-public class ErrorControllerImpl {
+public class ErrorControllerImpl implements ErrorController {
+
+    private static final String PATH = "/api/error";
+
+    @GetMapping(value = PATH)
+    public String error(){
+        return "Error handling";
+    }
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
 }
