77132b7468e149b2d9684c8b253b306ea55e8085
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-10-16 15:44:38 +0700

    Added bash script Build && Start

---
+++ src/main/java/com/saspallow/demo/controller/ErrorControllerImpl.java
@@ -0,0 +1,7 @@
+package com.saspallow.demo.controller;
+
+/**
+ * Created by dt213142 on 10/16/2017.
+ */
+public class ErrorControllerImpl {
+}
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ mvn clean install && java -jar target/demo-0.0.1-SNAPSHOT.jar
