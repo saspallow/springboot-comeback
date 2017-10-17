eaa44d84c60b13422658984877924d5f8203699b
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-10-17 11:54:40 +0700

    Added Test Suite.

--- src/test/java/com/saspallow/demo/MyControllerTest.java
+++ src/test/java/com/saspallow/demo/MyControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,7 +20,7 @@ import static org.junit.Assert.assertThat;
  * Created by dt213142 on 10/16/2017.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyControllerTest {
 
     @LocalServerPort
@@ -39,7 +40,7 @@ public class MyControllerTest {
     public void getUser() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody(), equalTo("{\"username\":\"Surasak\",\"age\":1,\"address\":\"Bangkok, Thailand\"}"));
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
     }
 
 }
---
+++ src/test/java/com/saspallow/demo/SuiteTest.java
@@ -0,0 +1,16 @@
+package com.saspallow.demo;
+
+/**
+ * Created by dt213142 on 10/17/2017.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    DemoApplicationTests.class,
+        MyControllerTest.class
+})
+public class SuiteTest {
+}
