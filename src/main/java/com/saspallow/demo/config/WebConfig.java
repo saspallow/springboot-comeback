174e6108053c81518f96bac6e760783c9f572fd9
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-10-17 14:04:05 +0700

    Added Configure response content type application/json;charset=UTF-8

---
+++ src/main/java/com/saspallow/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.saspallow.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by dt213142 on 10/17/2017.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false)
+                .favorParameter(true)
+                .defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+
+    }
+}
---
+++ src/test/java/com/saspallow/demo/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.saspallow.demo;
+
+/**
+ * Created by dt213142 on 10/17/2017.
+ */
+public class UserControllerTest {
+}
